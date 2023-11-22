60e6f19e4e0ad90ecddbb943a11ce9af8d38280a
Joaquin Ogallar <89165527+gogapy@users.noreply.github.com>
2023-11-22 13:53:37 -0300

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Board p = new Board();
-        p.setBoardDimension(10, 10);
-        p.generateNumbers();
-        p.start(p.setStartPoint(0, 0));
+        GreedyAlgorithm ga = new GreedyAlgorithm();
+        ga.setBoardDimension(10, 10);
+        ga.generateNumbers();
+        ga.start(ga.setStartPoint(0, 0));
         System.err.println("--------------------");
-        System.out.println("Total Points: " + p.getPoints());
+        System.out.println("Total Points: " + ga.getPoints());
         System.err.println("--------------------");
     }
 }
