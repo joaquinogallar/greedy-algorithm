6f37debc450e3432727657dfcfb9046f9e7f5401
Joaquin Ogallar <89165527+gogapy@users.noreply.github.com>
2023-11-22 13:40:31 -0300

    Update Main.java

--- source/Main.java
+++ source/Main.java
@@ -1,1 +1,11 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Board p = new Board();
+        p.setBoardDimension(10, 10);
+        p.generateNumbers();
+        p.start(p.setStartPoint(0, 0));
+        System.err.println("--------------------");
+        System.out.println("Total Points: " + p.getPoints());
+        System.err.println("--------------------");
+    }
+}
